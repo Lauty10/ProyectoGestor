ad6d03a5686dfc618ff1a59f94678b949a499b8f
Lautaro Maldonado <lautylbs@hotmail.com>
2024-10-16 19:21:40 -0300

    eliminando archivo

---
+++ ClassVendedores.h
@@ -0,0 +1,61 @@
+#ifndef CLASSVENDEDORES_H_INCLUDED
+#define CLASSVENDEDORES_H_INCLUDED
+#include <string.h>
+
+class Vendedores{
+private:
+    int idVendedor;
+    char nombre[30];
+    int dni;
+    char correo[35];
+    char clave[20];
+    char rol[6];
+public:
+    Vendedores(int i=0, const char* n="XXX", int d=1000000,const char * c="usuariox@gmail.com",const char* claveV="123456789",const char* r="seller"){
+    this->idVendedor=i;
+    strcpy(this->nombre,n);
+    this->dni=d;
+    strcpy(this->correo,c);
+    strcpy(this->clave,claveV);
+    strcpy(this->rol,r);
+    }
+    void setId(int idV){
+    this->idVendedor=idV;
+    };
+    void setNombre(char nombre[30]){
+    strcpy(this->nombre,nombre);
+    };
+    void setDni(int dni){
+    this->dni=dni;
+    };
+    void setCorreo(char correo[35]){
+    strcpy(this->correo,correo);
+    };
+    void setClave(char clave[20]){
+    strcpy(this->clave,clave);
+    };
+    void setRol(char rol[6]){
+    strcpy(this->rol,rol);
+    };
+    int getIdVendedor(){
+    return idVendedor;
+    }
+    const char* getNombre(){
+    return nombre;
+    }
+    int getDni(){
+    return dni;
+    }
+    const char* getCorreo(){
+    return correo;
+    }
+    const char* getClave(){
+    return clave;
+    }
+    const char* getRol(){
+    return rol;
+    }
+
+};
+
+#endif // CLASSVENDEDORES_H_INCLUDED
