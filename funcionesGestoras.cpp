670c3bf2d8f29b901b31877bb7f7be1bb97b4c0e
Lautaro Maldonado <lautylbs@hotmail.com>
2024-10-20 16:31:07 -0300

    agregando funcionalidad de registrarse

---
+++ funcionesGestoras.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "ClassVendedores.h"
+#include "Funciones.h"
+
+
+using namespace std;
+
+void menu(){
+int opcion;
+while(true){
+cout<<"BIENVENIDO AL SISTEMA GESTOR DE LA CONSESIONARIA"<<endl;
+cout<<"-------------------------------------------------------------"<<endl;
+cout<<"1-)INICIAR SESION"<<endl;
+cout<<"-------------------------------------------------------------"<<endl;
+cout<<"2-)REGISTRARSE"<<endl;
+cout<<"-------------------------------------------------------------"<<endl;
+cout<<"3-)RECUPERAR CLAVE"<<endl;
+cout<<"-------------------------------------------------------------"<<endl;
+cout<<"SALIR DEL SISTEMA"<<endl;
+cout<<"-------------------------------------------------------------"<<endl;
+cout<<"INGRESE LA OPCION QUE DESEE:";
+cin>>opcion;
+switch(opcion){
+case 1:
+    system("cls");
+    break;
+case 2:
+    system("cls");
+    registrarse();
+    break;
+case 3:
+    system("cls");
+    break;
+case 4:
+    return;
+default:
+    cout<<"Opcion invalida...";
+}
+}
+}
+//REGISTRARSE EN EL SISTEMA
+void registrarse(){
+FILE *registro;
+registro=fopen("C:\\Users\\Lauty\\OneDrive\\Escritorio\\ProyectoGestor\\registros.dat","ab");
+if(registro==nullptr){
+    cout<<"ERROR AL CREAR UN ARCHIVO DE REGISTROS"<<endl;
+}
+Vendedores obj;
+  char nombre[30];
+  int dni;
+  char correo[35];
+  char clave[20];
+cout<<"Ingrese su nombre:";
+cin.ignore();
+cin.getline(nombre,30,'\n');
+obj.setNombre(nombre);
+cout<<"Ingrese su dni:";
+cin>>dni;
+obj.setDni(dni);
+cout<<"Ingrese su correo:";
+cin.ignore();
+cin.getline(correo,35,'\n');
+obj.setCorreo(correo);
+cout<<"Ingrese su clave:";
+cin.ignore();
+cin.getline(clave,20,'\n');
+obj.setClave(clave);
+fwrite(&obj,sizeof(Vendedores),1,registro);
+system("cls");
+cout<<"Usuario dado de alta..."<<endl;
+system("pause");
+system("cls");
+fclose(registro);
+}
